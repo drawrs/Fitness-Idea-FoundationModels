0c72288022e8cb7de45b5b905ece199e9f4104a1
Rizal Hilman <rizal.hilman67@gmail.com>
2025-11-13 14:56:00 +0700

    Initial Commit

---
+++ Fitness Idea/Fitness_IdeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_IdeaApp.swift
+//  Fitness Idea
+//
+//  Created by Rizal Hilman on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_IdeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
